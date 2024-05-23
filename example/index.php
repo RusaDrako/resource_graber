9e7f068e8e96cac9aeed3782885ef080599ded35
Leonid Petukhov <rusadrako@yandex.ru>
2024-05-23 14:01:04 +0300

    example

---
+++ example/index.php
@@ -0,0 +1,36 @@
+<pre><?php
+
+use RusaDrako\mp3_graber\Graber;
+
+# Загрузка библиотеки
+require_once('../src/autoload.php');
+
+# Обработчик массива url
+$func = function($data){
+	$result = [];
+	foreach($data as $k => $v){
+		$result[$k] = "https://www.google.com/{$v}";
+	}
+	return $result;
+};
+
+# Настройка обработки хоста
+$host_settings = array(
+	"www.google.com" => array(
+		"handler" => array(
+			"regex" => '/src="([^"]*)"/umi',
+			"handler" => $func,
+		),
+		"safe_file" => array(
+			"use_key_for_file_name" => true,
+		),
+	),
+);
+
+# Формирование объекта
+$graber = new Graber();
+$graber->setTimeLimit(600);
+$graber->setHostSettings($host_settings);
+
+# Выполние загрузки
+$graber->execute("https://www.google.com/", "/test/");
